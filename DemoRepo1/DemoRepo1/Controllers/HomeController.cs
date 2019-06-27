a2a69c7330f1d6400ec53f4215e5acdb35e555f3
anjibabu420 <anji.vtalent@gmail.com>
2019-06-27 16:48:01 +0530

    home controller added

---
+++ DemoRepo1/DemoRepo1/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoRepo1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //Code Added in VS
+            return View();
+        }
+    }
+}
